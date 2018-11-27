d1ecc851b27f3cdda7496be9615a1332b1cc60fc
sysrichn <45215939+sysrichn@users.noreply.github.com>
2018-11-27 13:06:19 -0500

    Added Device Code

---
+++ DevKit/Device/.build/core/WMath.cpp.d
@@ -0,0 +1,4 @@
+c:\Users\richn\Documents\IoTWorkbenchProjects\examples\devkit_getstarted\Device\.build\core\WMath.cpp.o: \
+ C:\Users\richn\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.5.1\cores\arduino\WMath.cpp \
+ C:\Users\richn\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.5.1\cores\arduino/system/mbed_config.h \
+ C:\Users\richn\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.5.1\cores\arduino\WMath.h
---
+++ DevKit/Device/.build/core/parson/parson.c.d
@@ -0,0 +1,4 @@
+c:\Users\richn\Documents\IoTWorkbenchProjects\examples\devkit_getstarted\Device\.build\core\parson\parson.c.o: \
+ C:\Users\richn\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.5.1\cores\arduino\parson\parson.c \
+ C:\Users\richn\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.5.1\cores\arduino/system/mbed_config.h \
+ C:\Users\richn\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.5.1\cores\arduino\parson\parson.h
